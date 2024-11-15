0c6f01754b04c862ceab46790bdbb2b1235b184c
Nathaly Alves <nathalyalves0209@gmail.com>
2024-11-14 23:37:55 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Luz Noceda",
+        descricao: "Luz Noceda é uma adolescente humana com uma paixão por tudo que é mágico, Luz se encontra acidentalmente nas Ilhas Escaldadas, um mundo habitado por bruxas, demônios e outras criaturas fantásticas..",
+        link: "https://the-owl-house-brasil.fandom.com/pt-br/wiki/Luz_Noceda",
+        tags: "humana luzura magia"
+    },
+    {
+        titulo: "Amity Blight",
+        descricao: "Amity Blight é uma jovem bruxa prodigiosa que frequenta a Escola de Magia e Ocultismo de Hexside. Embora ela tenha sido originalmente descrita como uma antagonista, Amity mostrou tons mais claros à sua natureza após se ligar à.",
+        link: "https://the-owl-house-brasil.fandom.com/pt-br/wiki/Amity_Blight",
+        tags: "bruxa antagonista"
+    },
+    {
+        titulo: "Willow Park",
+        descricao: "Willow é naturalmente muito talentosa em magia de plantas, mas desde que ela foi forçada a seguir a trilha das abominações, muitos presumiram que ela era incompetente.",
+        link: "https://the-owl-house-brasil.fandom.com/pt-br/wiki/Willow_Park",
+        tags: "plantas amiga"
+    }
+];
